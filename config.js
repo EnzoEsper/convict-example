a48e7b4bcd49b28199e5ba51a3dc5880ee6ab4cc
Enzo Esperguin <enzoesper@hotmail.com>
2019-12-27 08:17:51 -0300

    custom schema

--- config.js
+++ config.js
@@ -1,37 +1,67 @@
 // config.js
-require('dotenv').config();
-const convict = require('convict');
+require("dotenv").config();
+const convict = require("convict");
 
-const config = convict({
- env: {
-   format: ['prod', 'dev', 'test'],
-   default: 'dev',
-   arg: 'nodeEnv',
-   env: 'NODE_ENV'
- },
- apple: {
-   format: String,
-   default: 'from default',
-   arg: 'apple',
-   env: 'APPLE'
- },
- banana: {
-   format: String,
-   default: 'from default',
-   arg: 'banana',
-   env: 'BANANA'
- },
- cherry: {
-   format: String,
-   default: 'from default',
-   arg: 'cherry',
-   env: 'CHERRY'
- }
+const schema = {
+  sources: {
+    doc: "A collection of credentials.",
+    format: "credential-array",
+    default: [],
+
+    children: {
+      username: {
+        username: "The username credential",
+        format: String,
+        default: null
+      },
+      password: {
+        doc: "The password credential",
+        format: String,
+        default: null
+      }
+    }
+  }
+};
+
+convict.addFormat({
+  name: "credential-array",
+  validate: function(credentials, schema) {
+    if (!Array.isArray(credentials)) {
+      throw new Error("must be of type Array");
+    }
+
+    for (credential of credentials) {
+      convict(schema.children)
+        .load(credential)
+        .validate();
+    }
+  }
 });
 
-const env = config.get('env');
-config.loadFile(`./config/${env}.json`);
+const config = convict({
+  sources: {
+    doc: "A collection of credentials.",
+    format: "credential-array",
+    default: [],
 
-config.validate({ allowed: 'strict' }); // throws error if config does not conform to schema
+    children: {
+      username: {
+        username: "The username credential",
+        format: String,
+        default: null
+      },
+      password: {
+        doc: "The password credential",
+        format: String,
+        default: null
+      }
+    }
+  }
+});
+
+// const env = config.get("env");
+config.loadFile(`./config/prod.json`);
+
+// config.validate({ allowed: "strict" }); // throws error if config does not conform to schema
 
 module.exports = config.getProperties(); // so we can operate with a plain old JavaScript object and abstract away convict (optional)
